dae9c73c1129d5126da1aa991caf9d1b1853af40
Noé Falzon <falzon.noe@gmail.com>
2018-10-06 17:08:43 +0300

    Keep only first move

--- lua/negamax.lua
+++ lua/negamax.lua
@@ -1,63 +1,27 @@
-local function negamax(valueFunction, childrenFunction, depth, alpha, beta, node)
-	
-	local children = childrenFunction(node)
-	
-	if depth == 0 or #children == 0 then
-		return valueFunction(node), {}
-	end
-	
-	local value = -math.huge
-	local moves
-	
-	for _,pair in ipairs(children) do
-		
-		local move, child = pair[1], pair[2]
-		
-		local childValue, nextMoves = negamax(valueFunction, childrenFunction, depth - 1, -beta, -alpha, child)
-		
-		childValue = -childValue
-		nextMoves[#nextMoves + 1] = move
-		
-		if childValue > value then
-			value = childValue
-			moves = nextMoves
-		end
-		
-		alpha = math.max(childValue, alpha)
-		
-		if alpha >= beta then
-			break
-		end		
-	end
-	
-	return value, moves
-end
-
 local function negamaxInPlace(valueFunction, movesFunction, applyMove, undoMove, depth, alpha, beta, node)
 	
 	local moves = movesFunction(node)
 	
 	if depth == 0 or #moves == 0 then
-		return valueFunction(node), {}
+		return valueFunction(node)
 	end
 	
 	local value = -math.huge
-	local bestMoves
+	local bestMove
 	
 	for _,move in ipairs(moves) do
 		
 		applyMove(node, move)
 		
-		local childValue, nextMoves = negamaxInPlace(valueFunction, movesFunction, applyMove, undoMove, depth - 1, -beta, -alpha, node)
+		local childValue = negamaxInPlace(valueFunction, movesFunction, applyMove, undoMove, depth - 1, -beta, -alpha, node)
 		
 		undoMove(node, move)
 
 		childValue = -childValue
-		nextMoves[#nextMoves + 1] = move
 		
 		if childValue > value then
 			value = childValue
-			bestMoves = nextMoves
+			bestMove = move
 		end
 		
 		alpha = math.max(childValue, alpha)
@@ -67,12 +31,11 @@ local function negamaxInPlace(valueFunction, movesFunction, applyMove, undoMove,
 		end		
 	end
 	
-	return value, bestMoves
+	return value, bestMove
 end
 
 
 return
 {
-	negamax = negamax,
-	negamaxInPlace = negamaxInPlace
+	negamax = negamaxInPlace
 }
