1eccef1874e57c23a2d493509d5edb712ad9a930
Noé Falzon <noe@yousician.com>
2019-07-26 12:10:56 +0300

    Benchmark program

--- onitama-cs/Program.cs
+++ onitama-cs/Program.cs
@@ -6,27 +6,15 @@ public static class Program
 	static public void Main()
 	{
 		var board = Board.FromString("ttTtt ..... ..... ...B. bb.bb");
-		var cards = CardState.FromNames("Tiger", "Ox", "Crane", "Rooster", "Horse");
+		var cards = CardState.FromNames("Tiger", "Ox", "Crane", "Horse", "Rooster");
 
 		var game = new GameState(board, cards, Player.Top);
 
 		Console.WriteLine(game);
 
+		var solver = new Solver(18, null, ttSize: 2);
+		solver.Start(game);
 
-		while(true)
-		{
-			var solver = new Solver(15, null, ttSize: 2);
-			solver.Start(game);
-
-			var m = solver.PrincipalVariation()[0];
-
-			Console.WriteLine(m);
-			game = game.ApplyMove(m);
-
-			Console.WriteLine(game);
-
-			if (game.board.TopWon() || game.board.BottomWon())
-				break;
-		}
+		solver.Stats.Print();
 	}
 }
