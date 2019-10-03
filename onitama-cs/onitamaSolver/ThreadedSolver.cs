51e68f1232dfc46e9560b2348375199bce45dcea
Noé Falzon <noe@yousician.com>
2019-10-03 09:52:28 +0300

    Lazy SMP but all threads basically follow the first one

--- onitama-cs/onitamaSolver/ThreadedSolver.cs
+++ onitama-cs/onitamaSolver/ThreadedSolver.cs
@@ -22,6 +22,8 @@ namespace Onitama
 
 		public int ComputeValue(GameState state, int depth, out Move bestMove)
 		{
+			var start = DateTime.Now;
+
 			var tasks = new Task[solvers.Count];
 			var moves = new Move[solvers.Count];
 			var values = new int[solvers.Count];
@@ -33,27 +35,23 @@ namespace Onitama
 				{
 					values[index] = solvers[index].ComputeValue(state, depth, out Move thisBestMove);
 					moves[index] = thisBestMove;
+
+					Console.WriteLine(thisBestMove + " " + values[index] + " " + (DateTime.Now - start));
 				});
 
 				tasks[index] = task;
 				task.Start();
 			}
-
-			/*
+			
 			var winner = Task.WaitAny(tasks);
 
 			foreach (var solver in solvers)
 				solver.Interrupt();
-			*/
 
-		   Task.WaitAll(tasks);
+			Task.WaitAll(tasks);
 
-			bestMove = moves[0];
-
-			for (int i = 0; i < solvers.Count; i++)
-				Console.WriteLine(moves[i] + " " + values[i]);
-
-			return values[0];
+			bestMove = moves[winner];
+			return values[winner];
 		}
 	}
 }
